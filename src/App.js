41201a613e27c57dc21cd23ef8f49c56a448c7e6
alexey <kutalo84@gmail.com>
2020-11-24 05:16:04 -0800

    Add preloader

--- src/App.js
+++ src/App.js
@@ -1,20 +1,23 @@
 import React, {Component} from 'react';
-import { BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 /*-----pages-----*/
 import {Homefrelencer} from './Home-frelencer';
 
-class App extends Component{
-
-  render(){
-    return(
-        <React.Fragment>
-          <Router>
-            <Switch>
-              <Route exact path="/" component={Homefrelencer}/>
-            </Switch>
-          </Router>
-        </React.Fragment>
-    );
-  }
+class App extends Component {
+    componentDidMount() {
+        this.props.hideLoader();
+    }
+    render() {
+        return (
+            <React.Fragment>
+                <Router>
+                    <Switch>
+                        <Route exact path="/" component={Homefrelencer}/>
+                    </Switch>
+                </Router>
+            </React.Fragment>
+        );
+    }
 }
+
 export default App;
--- src/index.js
+++ src/index.js
@@ -9,13 +9,24 @@ import './assets/elagent-icon/style.css';
 import './assets/etline-icon/style.css';
 import './assets/flaticon/flaticon.css';
 import './assets/css/animate.css';
-import "slick-carousel/slick/slick.css"; 
+import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import './assets/css/style.css';
 import './assets/css/responsive.css';
 import 'bootstrap/dist/js/bootstrap.min.js';
 import 'popper.js';
-ReactDOM.render(<App />, document.getElementById('root'));
+
+const loader = document.querySelector("#preloader");
+const hideLoader = () => loader.classList.add("loader--hide");
+
+setTimeout(
+    () =>
+        ReactDOM.render(
+            <App hideLoader={hideLoader} />,
+            document.getElementById("root")
+        ),
+    1400
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
