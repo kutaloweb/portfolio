d6e19ef142c8fed91031b7b4d9e5dba2aa06c309
alexey <kutalo84@gmail.com>
2020-12-10 18:30:40 -0800

    Add Typewriter to the header

--- src/components/BannerHeader.js
+++ src/components/BannerHeader.js
@@ -1,33 +1,55 @@
-import React, {Component} from 'react';
+import React from 'react';
 import Reveal from 'react-reveal/Reveal/';
 import WaterWave from 'react-water-wave';
+import useTypewriter from "react-typewriter-hook"
+import {useState, useEffect, useRef} from "react";
 
-class Banner extends Component {
-    render() {
-        return (
-            <section id="home">
-                <WaterWave interactive={false} strength={0} className="banner_area banner_shap_two"
-                           style={{width: '100%', height: '100%', backgroundSize: 'cover'}}
-                           imageUrl={require("../image/man.jpg")}>
-                    {() => (
-                        <div className="container">
-                            <div className="banner_content text-left">
-                                <Reveal effect="fadeInUp">
-                                    <h5>HELLO</h5>
-                                </Reveal>
-                                <Reveal effect="fadeInUp" duration={1500}>
-                                    <h2 className="wow fadeInLeft animated">I am Alexey Kutalo</h2>
-                                </Reveal>
-                                <Reveal effect="fadeInUp" duration={2200}>
-                                    <h4 className="wow fadeInUp anoimated">Visual Designer & Front-end Developer</h4>
-                                </Reveal>
-                            </div>
+const MagicOcean = [
+    "Visual Designer & Front-end Developer",
+    "Git/Github flow",
+    "React, Redux, React-router",
+];
+let index = 0;
+
+function BannerHeader() {
+    const [magicName, setMagicName] = useState("Visual Designer & Front-end Developer");
+    const intervalRef = useRef({});
+    const name = useTypewriter(magicName);
+    useEffect(
+        () => {
+            intervalRef.current = setInterval(() => {
+                index = index > 2 ? 0 : ++index;
+                setMagicName(MagicOcean[index]);
+            }, 5000);
+            return function clear() {
+                clearInterval(intervalRef.current);
+            };
+        },
+        [magicName]
+    );
+    return (
+        <section id="home">
+            <WaterWave interactive={false} strength={0} className="banner_area banner_shap_two"
+                       style={{width: '100%', height: '100%', backgroundSize: 'cover'}}
+                       imageUrl={require("../image/man.jpg")}>
+                {() => (
+                    <div className="container">
+                        <div className="banner_content text-left">
+                            <Reveal effect="fadeInUp">
+                                <h5>HELLO</h5>
+                            </Reveal>
+                            <Reveal effect="fadeInUp" duration={1500}>
+                                <h2 className="wow fadeInLeft animated">I am Alexey Kutalo</h2>
+                            </Reveal>
+                            <Reveal effect="fadeInUp" duration={2200}>
+                                <h4 className="wow fadeInUp anoimated cursor">{name} &zwnj;</h4>
+                            </Reveal>
                         </div>
-                    )}
-                </WaterWave>
-            </section>
-        );
-    }
+                    </div>
+                )}
+            </WaterWave>
+        </section>
+    );
 }
 
-export default Banner;
+export default BannerHeader;
